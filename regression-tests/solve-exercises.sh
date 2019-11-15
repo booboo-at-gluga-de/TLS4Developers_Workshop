493a0129fe97ae053b402516d120a8f48fd44c04
Bernd Stroessenreuther <booboo@gluga.de>
2019-11-15 23:50:21 +0100

    added more regression tests solve-exercises.sh: colored output for better readability solve-exercises.sh: messages about success / error of each single command

--- regression-tests/solve-exercises.sh
+++ regression-tests/solve-exercises.sh
@@ -1,89 +1,126 @@
 #!/bin/bash
 
-echo '::: ###################################################################'
-echo '::: solving exercise A.1'
-echo '::: ###################################################################'
+HEADLINE_COLOR='\033[1;34m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+ORANGE='\033[0;33m'
+NO_COLOR='\033[0m'
 
-echo '::: generating key'
-openssl genrsa -out /home/vagrant/example.com.key 2048
+function success {
+    echo -e "::: Result: ${GREEN}OK${NO_COLOR}"
+}
 
-echo '::: generating CSR'
-openssl req -new -key /home/vagrant/example.com.key -out /home/vagrant/example.com.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=example.com/emailAddress=certificates@example.com"
+function error {
+    echo -e "::: Result: ${RED}ERROR${NO_COLOR}"
+}
 
-echo '::: generating certificate'
-openssl x509 -req -days 365 -in /home/vagrant/example.com.csr -signkey /home/vagrant/example.com.key -out /home/vagrant/example.com.crt
+echo -e ":::"
+echo -e "::: ###################################################################"
+echo -e "::: ${HEADLINE_COLOR}solving exercise A.1${NO_COLOR}"
+echo -e "::: ###################################################################"
+echo -e ":::"
 
-echo '::: Apache config exercise-A1.conf'
-sudo cp /vagrant/exercises/A1/apache_conf.d/exercise-A1.conf /etc/httpd/conf.d/
+echo -e "::: ${HEADLINE_COLOR}generating key${NO_COLOR}"
+openssl genrsa -out /home/vagrant/example.com.key 2048 && success || error
 
-echo '::: restarting Apache'
-sudo systemctl restart httpd
+echo -e "::: ${HEADLINE_COLOR}generating CSR${NO_COLOR}"
+openssl req -new -key /home/vagrant/example.com.key -out /home/vagrant/example.com.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=example.com/emailAddress=certificates@example.com" && success || error
 
-echo '::: ###################################################################'
-echo '::: solving exercise A.2'
-echo '::: ###################################################################'
+echo -e "::: ${HEADLINE_COLOR}generating certificate${NO_COLOR}"
+openssl x509 -req -days 365 -in /home/vagrant/example.com.csr -signkey /home/vagrant/example.com.key -out /home/vagrant/example.com.crt && success || error
 
-echo '::: generating key'
-openssl genrsa -out /home/vagrant/localhost.key 2048
+echo -e "::: ${HEADLINE_COLOR}Apache config exercise-A1.conf${NO_COLOR}"
+sudo cp /vagrant/exercises/A1/apache_conf.d/exercise-A1.conf /etc/httpd/conf.d/ && success || error
 
-echo '::: generating CSR'
-openssl req -new -key /home/vagrant/localhost.key -out /home/vagrant/localhost.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=localhost/emailAddress=certificates@example.com"
+echo -e "::: ${HEADLINE_COLOR}restarting Apache${NO_COLOR}"
+sudo systemctl restart httpd && success || error
 
-echo '::: generating certificate'
-openssl x509 -req -days 365 -in /home/vagrant/localhost.csr -signkey /home/vagrant/localhost.key -out /home/vagrant/localhost.crt
+echo -e ":::"
+echo -e "::: ###################################################################"
+echo -e "::: ${HEADLINE_COLOR}solving exercise A.2${NO_COLOR}"
+echo -e "::: ###################################################################"
+echo -e ":::"
 
-echo '::: Apache config exercise-A2.conf'
-sudo cp /vagrant/exercises/A2/apache_conf.d/exercise-A2.conf /etc/httpd/conf.d/
+echo -e "::: ${HEADLINE_COLOR}generating key${NO_COLOR}"
+openssl genrsa -out /home/vagrant/localhost.key 2048 && success || error
 
-echo '::: restarting Apache'
-sudo systemctl restart httpd
+echo -e "::: ${HEADLINE_COLOR}generating CSR${NO_COLOR}"
+openssl req -new -key /home/vagrant/localhost.key -out /home/vagrant/localhost.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=localhost/emailAddress=certificates@example.com" && success || error
 
-echo '::: ###################################################################'
-echo '::: solving exercise A.3'
-echo '::: ###################################################################'
+echo -e "::: ${HEADLINE_COLOR}generating certificate${NO_COLOR}"
+openssl x509 -req -days 365 -in /home/vagrant/localhost.csr -signkey /home/vagrant/localhost.key -out /home/vagrant/localhost.crt && success || error
 
-echo '::: checking for previous created CA and removing it'
-[[ -d /home/vagrant/ca ]] && rm -Rf /home/vagrant/ca
+echo -e "::: ${HEADLINE_COLOR}Apache config exercise-A2.conf${NO_COLOR}"
+sudo cp /vagrant/exercises/A2/apache_conf.d/exercise-A2.conf /etc/httpd/conf.d/ && success || error
 
-echo '::: preparing config for the CA'
-/vagrant/exercises/A3/prepare_CA.sh /home/vagrant
+echo -e "::: ${HEADLINE_COLOR}restarting Apache${NO_COLOR}"
+sudo systemctl restart httpd && success || error
 
-echo '::: generating CA key'
-openssl genrsa -out /home/vagrant/ca/private/cacert.key 4096
+echo -e "::: ${HEADLINE_COLOR}creating localhost.keystore.p12${NO_COLOR}"
+openssl pkcs12 -export -in /home/vagrant/localhost.crt -inkey /home/vagrant/localhost.key -out /home/vagrant/localhost.keystore.p12 -passout pass:test && success || error
 
-echo '::: generating CA certificate'
-openssl req -config /home/vagrant/ca/ca.cnf -new -x509 -days 3650 -key /home/vagrant/ca/private/cacert.key -out /home/vagrant/ca/cacert.pem -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=Raffzahn CA/emailAddress=certificates@example.com"
+echo -e "::: ${HEADLINE_COLOR}creating localhost.truststore.p12${NO_COLOR}"
+openssl pkcs12 -export -in /home/vagrant/localhost.crt -nokeys -out /home/vagrant/localhost.truststore.p12 -passout pass:test && success || error
 
-echo '::: generating server key'
-openssl genrsa -out /home/vagrant/server.key 2048
+echo -e ":::"
+echo -e "::: ###################################################################"
+echo -e "::: ${HEADLINE_COLOR}solving exercise A.3${NO_COLOR}"
+echo -e "::: ###################################################################"
+echo -e ":::"
 
-echo '::: generating CSR'
-openssl req -new -key /home/vagrant/server.key -out /home/vagrant/server.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=localhost/emailAddress=certificates@example.com"
+echo -e "::: ${HEADLINE_COLOR}checking for previous created CA and removing it${NO_COLOR}"
+[[ -d /home/vagrant/ca ]] && rm -Rf /home/vagrant/ca && success || error
 
-echo '::: signing with CA certificate (creating server certificate)'
-openssl ca -batch -config /home/vagrant/ca/ca.cnf -extensions server_cert -days 365 -in /home/vagrant/server.csr -out /home/vagrant/server.crt
+echo -e "::: ${HEADLINE_COLOR}preparing config for the CA${NO_COLOR}"
+/vagrant/exercises/A3/prepare_CA.sh /home/vagrant && success || error
 
-echo '::: Apache config exercise-A3.conf'
-sudo cp /vagrant/exercises/A3/apache_conf.d/exercise-A3.conf /etc/httpd/conf.d/
+echo -e "::: ${HEADLINE_COLOR}generating CA key${NO_COLOR}"
+openssl genrsa -out /home/vagrant/ca/private/cacert.key 4096 && success || error
 
-echo '::: restarting Apache'
-sudo systemctl restart httpd
+echo -e "::: ${HEADLINE_COLOR}generating CA certificate${NO_COLOR}"
+openssl req -config /home/vagrant/ca/ca.cnf -new -x509 -days 3650 -key /home/vagrant/ca/private/cacert.key -out /home/vagrant/ca/cacert.pem -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=Raffzahn CA/emailAddress=certificates@example.com" && success || error
 
-echo '::: ###################################################################'
-echo '::: solving exercise A.4'
-echo '::: ###################################################################'
+echo -e "::: ${HEADLINE_COLOR}generating server key${NO_COLOR}"
+openssl genrsa -out /home/vagrant/server.key 2048 && success || error
 
-echo '::: generating client key'
-openssl genrsa -out /home/vagrant/client.key 2048
+echo -e "::: ${HEADLINE_COLOR}generating CSR${NO_COLOR}"
+openssl req -new -key /home/vagrant/server.key -out /home/vagrant/server.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=localhost/emailAddress=certificates@example.com" && success || error
 
-echo '::: generating CSR'
-openssl req -new -key /home/vagrant/client.key -out /home/vagrant/client.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=User Hans Wurst/emailAddress=hans.wurst@example.com"
+echo -e "::: ${HEADLINE_COLOR}signing with CA certificate (creating server certificate)${NO_COLOR}"
+openssl ca -batch -config /home/vagrant/ca/ca.cnf -extensions server_cert -days 365 -in /home/vagrant/server.csr -out /home/vagrant/server.crt && success || error
 
-echo '::: signing with CA certificate (creating client certificate)'
-openssl ca -batch -config /home/vagrant/ca/ca.cnf -extensions client_cert -days 365 -in /home/vagrant/client.csr -out /home/vagrant/client.crt
+echo -e "::: ${HEADLINE_COLOR}Apache config exercise-A3.conf${NO_COLOR}"
+sudo cp /vagrant/exercises/A3/apache_conf.d/exercise-A3.conf /etc/httpd/conf.d/ && success || error
 
-echo '::: Apache config exercise-A4.conf'
-sudo cp /vagrant/exercises/A4/apache_conf.d/exercise-A4.conf /etc/httpd/conf.d/
+echo -e "::: ${HEADLINE_COLOR}restarting Apache${NO_COLOR}"
+sudo systemctl restart httpd && success || error
 
-echo '::: restarting Apache'
-sudo systemctl restart httpd
+echo -e "::: ${HEADLINE_COLOR}creating server.keystore.p12${NO_COLOR}"
+openssl pkcs12 -export -in /home/vagrant/server.crt -inkey /home/vagrant/server.key -out /home/vagrant/server.keystore.p12 -passout pass:test && success || error
+
+echo -e "::: ${HEADLINE_COLOR}creating truststore.ca.p12${NO_COLOR}"
+openssl pkcs12 -export -in /home/vagrant/ca/cacert.pem -nokeys -out /home/vagrant/ca/truststore.ca.p12 -passout pass:test && success || error
+
+echo -e ":::"
+echo -e "::: ###################################################################"
+echo -e "::: ${HEADLINE_COLOR}solving exercise A.4${NO_COLOR}"
+echo -e "::: ###################################################################"
+echo -e ":::"
+
+echo -e "::: ${HEADLINE_COLOR}generating client key${NO_COLOR}"
+openssl genrsa -out /home/vagrant/client.key 2048 && success || error
+
+echo -e "::: ${HEADLINE_COLOR}generating CSR${NO_COLOR}"
+openssl req -new -key /home/vagrant/client.key -out /home/vagrant/client.csr -subj "/C=DE/ST=Franconia/L=Nuernberg/O=Raffzahn GmbH/CN=User Hans Wurst/emailAddress=hans.wurst@example.com" && success || error
+
+echo -e "::: ${HEADLINE_COLOR}signing with CA certificate (creating client certificate)${NO_COLOR}"
+openssl ca -batch -config /home/vagrant/ca/ca.cnf -extensions client_cert -days 365 -in /home/vagrant/client.csr -out /home/vagrant/client.crt && success || error
+
+echo -e "::: ${HEADLINE_COLOR}Apache config exercise-A4.conf${NO_COLOR}"
+sudo cp /vagrant/exercises/A4/apache_conf.d/exercise-A4.conf /etc/httpd/conf.d/ && success || error
+
+echo -e "::: ${HEADLINE_COLOR}restarting Apache${NO_COLOR}"
+sudo systemctl restart httpd && success || error
+
+echo -e "::: ${HEADLINE_COLOR}creating client.keystore.p12${NO_COLOR}"
+openssl pkcs12 -export -in /home/vagrant/client.crt -inkey /home/vagrant/client.key -out /home/vagrant/client.keystore.p12 -passout pass:test && success || error
