4143ba8e9ff5fce3a94580399abcc3be3d5b4235
Bernd Stroessenreuther <booboo@gluga.de>
2019-11-16 00:51:09 +0100

    run-all-tests.sh can be run with short output (commandline parameter -s)

--- regression-tests/run-all-tests.sh
+++ regression-tests/run-all-tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-goss --gossfile=/vagrant/regression-tests/goss-checks/goss.yaml validate --format documentation
+if [[ "$1" = "-s" ]]; then
+    REPORT_FORMAT=""
+else
+    REPORT_FORMAT="--format documentation"
+fi
+
+goss --gossfile=/vagrant/regression-tests/goss-checks/goss.yaml validate $REPORT_FORMAT
