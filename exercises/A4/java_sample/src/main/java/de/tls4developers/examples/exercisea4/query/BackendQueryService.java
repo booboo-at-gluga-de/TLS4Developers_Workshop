d9a4a1584e4302f7b03661afb2d7e0808151ab4c
Nico Krieg <n.krieg123@googlemail.com>
2020-02-03 13:19:06 +0100

    Adapt Java part of exercise A4 to REST backend.

--- exercises/A4/java_sample/src/main/java/de/tls4developers/examples/exercisea4/query/BackendQueryService.java
+++ exercises/A4/java_sample/src/main/java/de/tls4developers/examples/exercisea4/query/BackendQueryService.java
@@ -1,40 +1,38 @@
 package de.tls4developers.examples.exercisea4.query;
 
-import de.tls4developers.examples.exercisea4.data.Result;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @Service
+@Log4j2
 public class BackendQueryService {
 
-    private static final String SUCCESS_HEADER = "Eureka!";
-    private static final String SUCCESS_SUB_HEADER = "Query successful:";
-
-    private static final String ERROR_HEADER = "Oh, snap!";
-    private static final String ERROR_SUB_HEADER = "That hasn't worked:";
-
+    private final BackendProperties backendProperties;
     private final RestTemplate restTemplate;
 
-    @Value("${query-service-backend-url}")
-    private String backendUrl;
-
     @Autowired
-    public BackendQueryService(RestTemplate restTemplate) {
+    public BackendQueryService(BackendProperties backendProperties, RestTemplate restTemplate) {
+        this.backendProperties = backendProperties;
         this.restTemplate = restTemplate;
     }
 
-    public Result queryBackend() {
+    public Optional<String> queryBackend() {
 
         try {
-            ResponseEntity<String> responseEntity = restTemplate.getForEntity(backendUrl, String.class);
-            return new Result(SUCCESS_HEADER, SUCCESS_SUB_HEADER, responseEntity.getBody());
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(backendProperties.getUrl(), String.class);
+            if (responseEntity.getStatusCodeValue() == 200) {
+                return Optional.ofNullable(responseEntity.getBody());
+            }
         } catch (Exception e) {
-            e.printStackTrace();
-            return new Result(ERROR_HEADER, ERROR_SUB_HEADER, e.getMessage());
+            log.error("Something somewhere went terribly wrong upon attempt to query backend: ", e);
         }
+
+        return Optional.empty();
 
     }
 
